6201427614e4780aa9578db7fadd7f5716093730
wanghao8080 <wanghao@ninghao.net>
2023-01-06 18:08:41 +0800

    中间件里的请求：NextRequest

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
-const middleware = () => {
+import type { NextRequest } from 'next/server';
+
+const middleware = (request: NextRequest) => {
   console.log('🌵 中间件: middleware');
+  console.log(request.nextUrl.pathname);
 };
 
 export default middleware;
