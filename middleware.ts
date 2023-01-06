7a305fa05e76508007e36e9f609d2b2a927ef168
wanghao8080 <wanghao@ninghao.net>
2023-01-06 18:15:45 +0800

    在匹配的路径上运行中间件

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
 import type { NextRequest } from 'next/server';
 
+// export const config = {
+//   matcher: ['/posts/:id/'],
+// };
+
 const middleware = (request: NextRequest) => {
-  console.log('🌵 中间件: middleware');
-  console.log(request.nextUrl.pathname);
+  const {
+    nextUrl: { pathname },
+  } = request;
+
+  if (pathname.startsWith('/posts')) {
+    console.log('🌵 中间件: middleware');
+    console.log(request.nextUrl.pathname);
+  }
 };
 
 export default middleware;
