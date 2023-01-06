b2f5d8e6c2e9300a4db55aa9745fc0787f82fa51
wanghao8080 <wanghao@ninghao.net>
2023-01-06 15:19:52 +0800

    Next.js 基于文件系统的路由

---
+++ app/posts/head.tsx
@@ -0,0 +1,18 @@
+/**
+ * Head
+ */
+const Head = () => {
+  /**
+   * 视图
+   */
+  return (
+    <>
+      <title>内容列表 - 宁皓网</title>
+    </>
+  );
+};
+
+/**
+ * 导出 Head
+ */
+export default Head;
---
+++ app/posts/page.tsx
@@ -0,0 +1,14 @@
+/**
+ * Page
+ */
+const Page = () => {
+  /**
+   * 视图
+   */
+  return <div>内容列表</div>;
+};
+
+/**
+ * 导出 Page
+ */
+export default Page;
