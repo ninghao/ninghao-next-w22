52953605d489129da092448ccdf2896a97ee85a3
wanghao8080 <wanghao@ninghao.net>
2023-01-06 14:09:38 +0800

    Next.js 应用的页面（Page）

---
+++ pages/about.tsx
@@ -0,0 +1,19 @@
+/**
+ * Page
+ */
+const Page = () => {
+  /**
+   * 视图
+   */
+  return (
+    <div>
+      <h1>关于</h1>
+      <div>宁皓网创建于 2011 年。</div>
+    </div>
+  );
+};
+
+/**
+ * 导出 Page
+ */
+export default Page;
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * Page
+ */
+const Page = () => {
+  /**
+   * 视图
+   */
+  return (
+    <div>
+      <h1>首页</h1>
+      <div>我在宁皓网学习 Next.js。</div>
+    </div>
+  );
+};
+
+/**
+ * 导出 Page
+ */
+export default Page;
