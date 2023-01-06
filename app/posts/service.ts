b6debfbab64bf8d862eb2db96246af1e08c44252
wanghao8080 <wanghao@ninghao.net>
2023-01-07 07:40:10 +0800

    设置 Next.js 框架里的 fetch 请求回来的数据类型

--- app/posts/service.ts
+++ app/posts/service.ts
@@ -1,6 +1,7 @@
 import { appConfig } from '../config';
+import { PostList } from './type';
 
 export const getPosts = async () => {
   const response = await fetch(`${appConfig.apiBaseUrl}/posts`);
-  return response.json();
+  return response.json() as Promise<PostList>;
 };
