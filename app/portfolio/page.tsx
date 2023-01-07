5a2ef7b5e60951832967b3b404f4c7d9c5928e62
wanghao8080 <wanghao@ninghao.net>
2023-01-07 11:15:59 +0800

    懒加载图像（Lazy）

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import { CSSProperties } from 'react';
 import p1 from '/public/images/4612.jpg';
+import p2 from '/public/images/4293.jpg';
+import p3 from '/public/images/4423.jpg';
+import p4 from '/public/images/5122.jpg';
 
 /**
  * Page
@@ -15,6 +18,9 @@ const Page = () => {
     <div>
       <div>
         <Image src={p1} alt="" style={imageStyles} quality={80} />
+        <Image src={p2} alt="" style={imageStyles} quality={80} />
+        <Image src={p3} alt="" style={imageStyles} quality={80} />
+        <Image src={p4} alt="" style={imageStyles} quality={80} />
       </div>
     </div>
   );
