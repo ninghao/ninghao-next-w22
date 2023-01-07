d1f32afd729da082414d4d417d2c88b7355df8e6
wanghao8080 <wanghao@ninghao.net>
2023-01-07 16:19:54 +0800

    发布内容上传图像

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,18 +1,76 @@
 'use client';
 
+import Image from 'next/image';
 import { useCreatePost } from './hook';
 
 /**
  * Page
  */
 const Page = () => {
-  const { title, setTitle, content, setContent, createPost } = useCreatePost();
+  const {
+    title,
+    setTitle,
+    content,
+    setContent,
+    createPost,
+    setFile,
+    fileInput,
+    imagePreviewUrl,
+    setImagePreviewUrl,
+    createImagePreview,
+  } = useCreatePost();
 
   /**
    * 视图
    */
   return (
-    <div>
+    <div
+      onDragOver={(event) => event.preventDefault()}
+      onDrop={(event) => {
+        event.preventDefault();
+        const file = event.dataTransfer.files[0];
+
+        if (file) {
+          setFile(file);
+          createImagePreview(file);
+        }
+      }}
+    >
+      <div>
+        {imagePreviewUrl && (
+          <Image
+            src={imagePreviewUrl}
+            width={378}
+            height={378}
+            alt="图像预览"
+          />
+        )}
+      </div>
+      <div>
+        <button
+          className="bordered"
+          onClick={() => {
+            fileInput.current?.click();
+          }}
+        >
+          选择图像文件
+        </button>
+        <input
+          type="file"
+          hidden
+          ref={fileInput}
+          accept="image/png, image/jpeg, image/jpg"
+          onChange={({ currentTarget: { files } }) => {
+            if (files && files[0]) {
+              setFile(files[0]);
+              createImagePreview(files[0]);
+            } else {
+              setFile(null);
+              setImagePreviewUrl('');
+            }
+          }}
+        />
+      </div>
       <div>
         <input
           type="text"
