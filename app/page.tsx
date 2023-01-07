4c91eaaffe79aeea1b2a522f15fccaa883317d9d
wanghao8080 <wanghao@ninghao.net>
2023-01-07 09:37:57 +0800

    Next.js 应用里的服务端组件（Server Component）

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Link from 'next/link';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 /**
  * Page
  */
-const Page = () => {
+const Page = async () => {
+  const filePath = path.join('public', 'README.md');
+  const fileContent = await fs.readFile(filePath);
+
   /**
    * 视图
    */
   return (
     <div>
       <h1>首页</h1>
-      <div>我在宁皓网学习 Next.js。</div>
+      <div>{fileContent.toString()}</div>
       <div>
         <Link href="/about">关于我们 →</Link>
       </div>
